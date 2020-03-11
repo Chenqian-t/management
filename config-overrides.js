1d5ae1149b7f75b1d015320b43863ab19f0ccead
Chenqian-t <1473828153@qq.com>
2020-03-11 16:01:57 +0800

    创建react-app 并配置了antd以及less支持

---
+++ config-overrides.js
@@ -0,0 +1,22 @@
+// 基于customize-cra和react-app-rewired的定制化配置文件
+
+// 从customize-cra引入一些相关方法
+const { override, addLessLoader, fixBabelImports, addDecoratorsLegacy } = require('customize-cra');
+
+const theme = require('./theme');
+
+module.exports = override(
+    addLessLoader({
+        // 添加less支持
+        javascriptEnabled: true,
+        // antd的自定义主题
+        modifyVars: theme
+    }),
+    // 装饰器写法支持
+    addDecoratorsLegacy(),
+    fixBabelImports('import', {
+        libraryName: 'antd',
+        libraryDirectory: 'es',
+        style: true
+    })
+);

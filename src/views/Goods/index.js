8c7942bd4368575cb52b75b10889025704fad538
Chenqian-t <1473828153@qq.com>
2020-03-13 22:41:18 +0800

    '页面的权限设置（商品改、删部分）'

--- src/views/Goods/index.js
+++ src/views/Goods/index.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { Card, Table, Button } from 'antd';
+import { connect } from 'react-redux';
 
 import { getGoodsInfo } from '../../requests';
 import ButtonGroup from 'antd/lib/button/button-group';
@@ -7,7 +8,15 @@ import ButtonGroup from 'antd/lib/button/button-group';
 const displayName = { id: '编号', name: '名称', type: '类型', description: '描述', cost: '成本', price: '价格', sales: '销量' } 
 const displayWidth = { id: '10%', name: '8%', type: '8%', description: '30%', cost: '8%', price: '8%', sales: '8%' } 
 
-export default class Goods extends Component {
+const mapState = state => {
+    return {
+        role: state.user.role
+    }
+}
+
+
+@connect(mapState)
+class Goods extends Component {
     constructor() {
         super()
         this.state = {
@@ -29,8 +38,15 @@ export default class Goods extends Component {
             render: () => {
                 return (
                     <ButtonGroup>
-                        <Button>编辑</Button>
-                        <Button>下架</Button>
+                        {
+                            this.props.role === '05' ?
+                            <p style={{color: 'orange', textAlign: 'center'}}>对不起，您没有操作权限!</p>
+                            : <Button>编辑</Button>
+                        }
+                        {
+                            (this.props.role === '04') || (this.props.role === '05') ?
+                            null : <Button>下架</Button>
+                        }
                     </ButtonGroup>
                 )
             }
@@ -50,6 +66,7 @@ export default class Goods extends Component {
         this.getData()
     }
     render() {
+        console.log(this.props.role)
         return (
             <Card title='商品信息' bordered='false' >
                 <Table rowKey={record => record.id} columns={this.state.columns} dataSource={this.state.data} />
@@ -57,3 +74,5 @@ export default class Goods extends Component {
         )
     }
 }
+
+export default Goods
